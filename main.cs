e3de21a750aaf69e9ecb5d86e8e1382c46a47efc
Arseniy Polovinkin <polosenya123@mail.ru>
2024-05-09 03:08:38 +0400

    fs

--- main.cs
+++ main.cs
@@ -2,8 +2,14 @@ csharp
 using System; 
 
 class Program 
- 
+{ 
 static void Main() 
-
+{ 
 string[] inputArray = new string[] { "Hello", "2", "world", ":-)" }; 
 string[] resultArray = FilterArray(inputArray); 
+
+foreach (var item in resultArray) 
+{ 
+Console.Write(item + " "); 
+} 
+} 
