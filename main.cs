ad1f19420c6008c01208f3772eafa4b16589409f
Arseniy Polovinkin <polosenya123@mail.ru>
2024-05-09 03:09:52 +0400

    end

--- main.cs
+++ main.cs
@@ -23,3 +23,18 @@ if (input[i].Length <= 3)  // проверяем длину строки
 count++;  // увеличиваем счетчик 
 } 
 } 
+string[] result = new string[count];  // создаем новый массив нужной длины 
+
+count = 0;  // обнуляем счетчик 
+for (int i = 0; i < input.Length; i++) 
+{ 
+if (input[i].Length <= 3) 
+{ 
+result[count] = input[i];  // добавляем строку в новый массив 
+count++; 
+} 
+} 
+
+return result; 
+} 
+} 
