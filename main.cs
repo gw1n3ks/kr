c862512da6817f3c7a9f2149b30dc0cc8ff8a5ad
Arseniy Polovinkin <polosenya123@mail.ru>
2024-05-09 03:06:44 +0400

    creat

---
+++ main.cs
@@ -0,0 +1,9 @@
+csharp 
+using System; 
+
+class Program 
+ 
+static void Main() 
+
+string[] inputArray = new string[] { "Hello", "2", "world", ":-)" }; 
+string[] resultArray = FilterArray(inputArray); 
